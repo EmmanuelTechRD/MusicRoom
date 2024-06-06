e0c9902c4a01710cdfe957bbb5da321e507f297f
Emmanuel Soto <emmanueltechrd@gmail.com>
2024-06-06 16:01:36 -0400

    [ADD] New components

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views as v
 
 urlpatterns = [
-    path('', v.index, name='home')
+    path('', v.index, name='home'),
+    path('join', v.index, name='join_room'),
+    path('create', v.index, name='create_room')
 ]
